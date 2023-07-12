261ced979fedf0641a09aa6a21ecbf60741ecd08
Alessio Trivisonno <alessio.trivisonno@gmail.com>
2023-07-12 07:13:36 +0000

    feat: adds update configmap

--- pkg/proxy/reconcile/configmap.go
+++ pkg/proxy/reconcile/configmap.go
@@ -3,6 +3,7 @@ package reconcile
 import (
 	"context"
 	"fmt"
+	"reflect"
 
 	corev1 "k8s.io/api/core/v1"
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
@@ -10,42 +11,74 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/types"
 	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
 
 	"github.com/dexter0195/oauth2-proxy-operator/pkg/proxy"
 )
 
 func ConfigMap(ctx context.Context, params Params) error {
 
+	// Create the desirec ConfigMap object
+	desiredCM, err := desiredConfigMap(params)
+	if err != nil {
+		params.Log.Error(err, "Failed to define new ConfigMap resource for OAuth2Proxy")
+		return err
+	}
 	// check if configmap exists, if not create a new one
-	foundCM := &corev1.ConfigMap{}
+	currentCM := &corev1.ConfigMap{}
 	oauth2proxy := params.Instance
-	err := params.Client.Get(ctx, types.NamespacedName{Name: oauth2proxy.Name, Namespace: oauth2proxy.Namespace}, foundCM)
+	err = params.Client.Get(ctx, types.NamespacedName{Name: oauth2proxy.Name, Namespace: oauth2proxy.Namespace}, currentCM)
 	if err != nil && apierrors.IsNotFound(err) {
-		// Define a new configmap
-		cm, err := configMapForOAuth2Proxy(params)
-		if err != nil {
-			params.Log.Error(err, "Failed to define new ConfigMap resource for OAuth2Proxy")
+		// The following implementation will update the status
+		meta.SetStatusCondition(&oauth2proxy.Status.Conditions, metav1.Condition{Type: TypeAvailableOAuth2Proxy,
+			Status: metav1.ConditionFalse, Reason: "Reconciling",
+			Message: fmt.Sprintf("Failed to create ConfigMap for the custom resource (%s): (%s)", oauth2proxy.Name, err)})
 
-			// The following implementation will update the status
-			meta.SetStatusCondition(&oauth2proxy.Status.Conditions, metav1.Condition{Type: TypeAvailableOAuth2Proxy,
-				Status: metav1.ConditionFalse, Reason: "Reconciling",
-				Message: fmt.Sprintf("Failed to create ConfigMap for the custom resource (%s): (%s)", oauth2proxy.Name, err)})
-
-			if err := params.Client.Status().Update(ctx, oauth2proxy); err != nil {
-				params.Log.Error(err, "Failed to update OAuth2Proxy status")
-				return err
-			}
-		}
-		params.Log.Info("Creating a new ConfigMap", "ConfigMap.Namespace", cm.Namespace, "ConfigMap.Name", cm.Name)
-		if err := params.Client.Create(ctx, cm); err != nil {
-			params.Log.Error(err, "Failed to create new ConfigMap", "ConfigMap.Namespace", cm.Namespace, "ConfigMap.Name", cm.Name)
+		if err := params.Client.Status().Update(ctx, oauth2proxy); err != nil {
+			params.Log.Error(err, "Failed to update OAuth2Proxy status")
 			return err
 		}
+		params.Log.Info("Creating a new ConfigMap", "ConfigMap.Namespace", desiredCM.Namespace, "ConfigMap.Name", desiredCM.Name)
+		if err := params.Client.Create(ctx, desiredCM); err != nil {
+			params.Log.Error(err, "Failed to create new ConfigMap", "ConfigMap.Namespace", desiredCM.Namespace, "ConfigMap.Name", desiredCM.Name)
+			return err
+		}
+	} else {
+		// check if the desired configmap is different from the current configmap and merge the changes
+		updated := currentCM.DeepCopy()
+		if updated.Annotations == nil {
+			updated.Annotations = map[string]string{}
+		}
+		if updated.Labels == nil {
+			updated.Labels = map[string]string{}
+		}
+
+		updated.Data = desiredCM.Data
+		updated.BinaryData = desiredCM.BinaryData
+		updated.ObjectMeta.OwnerReferences = desiredCM.ObjectMeta.OwnerReferences
+
+		for k, v := range desiredCM.ObjectMeta.Annotations {
+			updated.ObjectMeta.Annotations[k] = v
+		}
+		for k, v := range desiredCM.ObjectMeta.Labels {
+			updated.ObjectMeta.Labels[k] = v
+		}
+
+		patch := client.MergeFrom(currentCM)
+
+		if configMapChanged(desiredCM, currentCM) {
+			params.Recorder.Event(updated, "Normal", "ConfigUpdate ", fmt.Sprintf("OpenTelemetry Config changed - %s/%s", desiredCM.Namespace, desiredCM.Name))
+			if err := params.Client.Patch(ctx, updated, patch); err != nil {
+				return fmt.Errorf("failed to apply changes: %w", err)
+			}
+		}
+
+		params.Log.V(2).Info("applied", "configmap.name", desiredCM.Name, "configmap.namespace", desiredCM.Namespace)
 	}
 	return nil
 }
 
-func configMapForOAuth2Proxy(params Params) (*corev1.ConfigMap, error) {
+func desiredConfigMap(params Params) (*corev1.ConfigMap, error) {
 	oauth2proxy := params.Instance
 	ls := proxy.LabelsForOAuth2Proxy(oauth2proxy.Name)
 
@@ -71,4 +104,8 @@ func configMapForOAuth2Proxy(params Params) (*corev1.ConfigMap, error) {
 	}
 
 	return cm, nil
+}
+
+func configMapChanged(desired *corev1.ConfigMap, actual *corev1.ConfigMap) bool {
+	return !reflect.DeepEqual(desired.Data, actual.Data)
 }
